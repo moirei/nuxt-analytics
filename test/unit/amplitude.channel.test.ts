6c3a5dd7b06e3a0bd2e7bedffe591518ca460046
Augustus <agstrscntw3@gmail.com>
2023-06-21 10:42:25 +0930

    chore: test amplitude channel

--- test/unit/amplitude.channel.test.ts
+++ test/unit/amplitude.channel.test.ts
@@ -1,27 +1,121 @@
-import { beforeEach, describe, expect, it, vi } from "vitest";
+import {
+  SpyInstance,
+  afterEach,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  vi,
+} from "vitest";
+import Amplitude from "../../src/runtime/channels/Amplitude";
 
-describe.todo("amplitude [channel]", () => {
-  beforeEach(() => {
-    //
+describe("amplitude [channel]", () => {
+  let channel: Amplitude;
+  let config: any;
+  let initSpy: SpyInstance;
+  let identifySpy: SpyInstance;
+  let setUserIdSpy: SpyInstance;
+  let logEventSpy: SpyInstance;
+  let setOptOutSpy: SpyInstance;
+  let fakeIndentity: { set: SpyInstance; get: SpyInstance };
+
+  beforeAll(() => {
+    const parentNode = {
+      insertBefore: () => void 0,
+    };
+    vi.spyOn(document, "getElementsByTagName").mockImplementation((): any => {
+      return [{ parentNode }];
+    });
   });
 
-  it("should install channel", () => {
-    //
+  beforeEach(async () => {
+    config = {
+      apiKey: "AMPLITUDE_KEY",
+    };
+    channel = new Amplitude("amplitude", config);
+
+    initSpy = vi.fn();
+    identifySpy = vi.fn();
+    setUserIdSpy = vi.fn();
+    logEventSpy = vi.fn();
+    setOptOutSpy = vi.fn();
+
+    fakeIndentity = {
+      set: vi.fn(),
+      get: vi.fn(),
+    };
+
+    const instance = {
+      init: initSpy,
+      identify: identifySpy,
+      setUserId: setUserIdSpy,
+      logEvent: logEventSpy,
+      setOptOut: setOptOutSpy,
+    };
+
+    vi.spyOn(channel, "getInstance").mockImplementation(() => instance);
+
+    await channel.install();
   });
 
-  it("should send track event", () => {
-    //
+  afterEach(async () => {
+    await channel.uninstall();
   });
 
-  it("should send page event", () => {
-    //
+  it("expects identify to set the distinct user ID, and calls amplitude with any extra user properties", async () => {
+    channel["identity"] = fakeIndentity;
+
+    await channel.identify({
+      user: {
+        id: "123",
+        email: "ben.simmons@sixers.net",
+      },
+    });
+
+    expect(setUserIdSpy).toHaveBeenCalledWith("123");
+    expect(fakeIndentity.set).toHaveBeenCalledWith(
+      "email",
+      "ben.simmons@sixers.net"
+    );
+    expect(identifySpy).toHaveBeenCalled();
   });
 
-  it("should send identify event", () => {
-    //
+  it("expects track to send the correct request shape", async () => {
+    const props = {
+      email: "joel.embiid@sixers.net",
+    };
+
+    await channel.track({
+      event: "Registered User",
+      props,
+    });
+
+    await channel.track({
+      event: "User Login",
+    });
+
+    expect(logEventSpy).toHaveBeenNthCalledWith(1, "Registered User", props);
+    expect(logEventSpy).toHaveBeenNthCalledWith(2, "User Login");
   });
 
-  it("should uninstall channel", () => {
-    //
+  it("expects page to send the correct request shape", async () => {
+    const props = {
+      url: "/shot-charts/missed-threes",
+    };
+
+    await channel.page({ props });
+
+    expect(logEventSpy).toHaveBeenCalledWith("Page View", props);
+  });
+
+  it("expects optOut to call amplitude optOut with true", async () => {
+    await channel.optOut();
+    expect(setOptOutSpy).toHaveBeenCalledWith(true);
+  });
+
+  it("expects optIn to call amplitude optOut with false", async () => {
+    await channel.optIn();
+    expect(setOptOutSpy).toHaveBeenCalledWith(false);
   });
 });
